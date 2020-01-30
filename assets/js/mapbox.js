b3db75b8655a78c166f61c9caf3d14a958ee5799
Piero <grilli.piero@circeco.org>
2020-01-30 11:30:06 +0000

    trying out with a copy and paste from mapbox

---
+++ assets/js/mapbox.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2lyY2VjbyIsImEiOiJjazRmZnBqbDAwbHpmM3RvMjhvOTdlbGJlIn0.fb-zFM7RFrt7rCUzE-TsyA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/circeco/ck5zjodry0ujw1ioaiqvk9kjs',
+  center: [59.325, 18.072],
+  zoom: 12
+});

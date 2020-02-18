5818fb4b82c490a715d8c842d0e80bb29963979a
Piero <grilli.piero@circeco.org>
2020-02-18 21:44:41 +0000

    nested js maplist

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,12 +1,18 @@
 
 // Add some text to explain and reference
 
+function pleaseBeGood() {
+    if ("@media screen and (max-width: 450px)")
+        return mapListBtn()
+}
+
+
 
 function mapListBtn() {
-  var x = document.getElementById("maplist");
-  if (x.style.width === "0%") {
-    x.style.width = "50%";
-  } else {
-    x.style.width = "0%";
-  }
+    var x = document.getElementById("maplist");
+    if (x.style.width === "0%") {
+        x.style.width = "50%";
+    } else {
+        x.style.width = "0%";
+    }
 }

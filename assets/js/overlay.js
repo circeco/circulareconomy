934365f1ffa45324b269566a638591d17507fb77
Piero <grilli.piero@circeco.org>
2020-02-18 10:03:46 +0000

    overlay for map-list in place but to better style

---
+++ assets/js/overlay.js
@@ -0,0 +1,10 @@
+
+// Add some text to explain and reference
+
+function openNav() {
+  document.getElementById("maplist").style.width = "50%";
+}
+
+function closeNav() {
+  document.getElementById("maplist").style.width = "0%";
+}

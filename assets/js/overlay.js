e13621671e5bb82670a0d8860d99fc9a8213e1e3
Piero <grilli.piero@circeco.org>
2020-02-18 22:24:29 +0000

    sizing done

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,12 +1,6 @@
 
 // Add some text to explain and reference
 
-function pleaseBeGood() {
-    if ("@media screen and (max-width: 450px)")
-        mapListBtn()
-}
-
-
 
 function mapListBtn() {
     var x = document.getElementById("maplist");

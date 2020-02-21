8880009cb92d3cb4d9404cfb6253ae4a63f8dac5
Piero <grilli.piero@circeco.org>
2020-02-21 21:53:39 +0000

    cleaning

--- assets/js/scroll-snap.js
+++ assets/js/scroll-snap.js
@@ -1,12 +1,10 @@
 
-const gra = function(min, max) {
+const gra = function (min, max) {
     return Math.random() * (max - min) + min;
 }
-const init = function(){
-	let items = document.getElementsByClassName('scroll');
-	for (let i = 0; i < items.length; i++){
-		items[i].style.background = randomColor({luminosity: 'light'});
-	}
-	cssScrollSnapPolyfill()
+
+let items = document.getElementsByClassName('scroll');
+for (let i = 0; i < items.length; i++) {
+    items[i].style.background = randomColor({ luminosity: 'light' });
 }
-init();
+cssScrollSnapPolyfill()

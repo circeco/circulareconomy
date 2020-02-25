a4549adc6fd5b5bb3e36287907e9ca859fe053ef
Piero <grilli.piero@circeco.org>
2020-02-25 16:03:07 +0000

    style issues with send button finally resolved

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,19 +7,17 @@ function sendMail(contactForm) {
     })
         .then(
             function (response) {
-                console.log("SUCCESS", response.status, response.text);
+                console.log("SUCCESS", response.status, response.text).reset();
             },
             function (error) {
                 console.log("FAILED", error);
             }
-        );
+        )
+        
     return false;  // To block from loading a new page
-    function clearForm() {
-        document.getElementById("contact-form").reset();
-    }
 }
 
-$(function () {
+$(function buttonStuff() {
     console.log('one')
     $("#send_button").click(function () {
         $("#send_button").addClass("onclic", 250, validate);
@@ -37,7 +35,11 @@ $(function () {
         setTimeout(function () {
             $("#send_button").removeClass("validate");
         }, 1250);
+        function clearForm() {
+            console.log('reset')
+            document.getElementById("contact-form").reset();
+        }
     }
-
 });
+
 
